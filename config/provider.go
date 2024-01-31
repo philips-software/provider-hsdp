9d980bf064cad5d0120ccc9b5bcee3482deba1be
Koen de Laat <koen.de.laat@philips.com>
2024-01-31 10:25:40 +0000

    Use goimports

--- config/provider.go
+++ config/provider.go
@@ -7,6 +7,7 @@ package config
 import (
 	// Note(turkenh): we are importing this to embed provider schema document
 	_ "embed"
+
 	"github.com/philips-software/terraform-provider-hsdp/hsdp"
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
